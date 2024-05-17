674a70c83f84b176e66e1bfa7b5671c4cfa0c657
amrg-08 <anamrg08@outlook.com>
2024-05-16 21:05:01 -0500

    Carpetas y estructuras

---
+++ Correa-Cecilia_Restrepo-Ana/Src/com/backend/entity/Odontologo.java
@@ -0,0 +1,39 @@
+package com.backend.entity;
+
+public class Odontologo {
+
+     private String nombre;
+     private String apellido;
+     private int matricula;
+
+
+     public Odontologo(String nombre, String apellido, int matricula) {
+          this.nombre = nombre;
+          this.apellido = apellido;
+          this.matricula = matricula;
+     }
+
+     public String getNombre() {
+          return nombre;
+     }
+
+     public String getApellido() {
+          return apellido;
+     }
+
+     public int getMatricula() {
+          return matricula;
+     }
+
+     public void setNombre(String nombre) {
+          this.nombre = nombre;
+     }
+
+     public void setApellido(String apellido) {
+          this.apellido = apellido;
+     }
+
+     public void setMatricula(int matricula) {
+          this.matricula = matricula;
+     }
+}
